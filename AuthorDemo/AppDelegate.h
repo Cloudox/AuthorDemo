558ffa31aef332506b7a4a127d2930fc30566941
Cloudox <cloudox@foxmail.com>
2015-12-21 15:57:48 +0800

    Initial Commit

---
+++ AuthorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AuthorDemo
+//
+//  Created by csdc-iMac on 15/12/21.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AuthorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AuthorDemo
+//
+//  Created by csdc-iMac on 15/12/21.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
